c9adcb269d2483a7af6e2f105026542a1d3ef593
Manster <manster@programmer.net>
2016-07-12 15:30:59 +0800

    Angular Ace Admin initialization

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularAceAdminApp
+ * @description
+ * # angularAceAdminApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularAceAdminApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource'
+  ]);
